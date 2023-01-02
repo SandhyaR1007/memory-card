0829ef2b299ccd75342a7a7687864a2557dc211b
SandhyaR1007 <47480884+SandhyaR1007@users.noreply.github.com>
2023-01-02 18:00:29 +0000

    styles: update styles for memory-board

---
+++ img.js
@@ -0,0 +1,10 @@
+export default const images = {
+  btLogo: "https://yt3.googleusercontent.com/ytc/AMLnZu9y9YRgvPJ595WqgJ8EfTNmUMozn77W5Jog2OWICg=s900-c-k-c0x00ffffff-no-rj",
+  chimmy: "https://www.pngitem.com/pimgs/m/537-5373089_chimmy-bt21-baby-bt21-wallpaper-baby-chimmy-hd.png",
+  "tata": "https://cdn141.picsart.com/331564295052211.png",
+  "kookie": "https://image.pngaaa.com/867/194867-middle.png",
+  "shooky": "https://i.pinimg.com/originals/0f/c6/5d/0fc65d76d3b6aa8704b3925e20f3b20f.png",
+  "koya": "https://i.pinimg.com/736x/91/3f/bd/913fbdf644301e42ca0ac10daf32a3db.jpg"
+
+
+}
